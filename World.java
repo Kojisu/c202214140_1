000b71ccddc933d0b1a593fe620a76b583e63212
Ko, Jisu <0918kojisu@gmail.com>
2025-09-17 00:35:03 +0900

    five

--- World.java
+++ World.java
@@ -2,5 +2,9 @@ public class World {
 
     public static void run() { 
         System.out.println("202214140"); 
-    } 
+    }
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
 } 
